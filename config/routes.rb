129d3b8d1fa36ebdf9a31772c45b4ba37ccbfc7d
Rahul Gupta <guptarahul70322@gmail.com>
2020-08-15 14:26:44 +0530

    important basic crud complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :articles, only: [:show, :index, :new, :create]
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+  resources :articles #, only: [:show, :index, :new, :create, :edit, :update, :destroy]
 end
