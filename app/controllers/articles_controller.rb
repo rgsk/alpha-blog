7858cc045d0d21ccc03f3a6868d5b89ba23b7fdf
Rahul Gupta <guptarahul70322@gmail.com>
2020-08-17 18:42:26 +0530

    restricted access from backend

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,10 @@
 class ArticlesController < ApplicationController 
   before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  before_action :require_user, except: [:show, :index]
+
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+
   def show
   end
   def index 
@@ -43,4 +48,10 @@ class ArticlesController < ApplicationController
     params.require(:article).permit(:title, :description)
   end
   
+  def require_same_user
+    if current_user != @article.user
+      flash[:alert] = "You can only edit or delete article created by you"
+      redirect_to @article
+    end
+  end
 end
