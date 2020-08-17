76d4d83309da94f915829bdfd1043738821b1c05
Rahul Gupta <guptarahul70322@gmail.com>
2020-08-17 17:25:23 +0530

    restrict user access from frontend

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,7 @@ class ArticlesController < ApplicationController
   end
   def create
     @article = Article.new(article_params)
-    @article.user = User.last
+    @article.user = current_user
     if @article.save
       flash[:notice] = "Article was created successfully"
       redirect_to @article
